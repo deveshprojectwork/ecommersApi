0b10fe454a633e4d2c41bdb0d105ec2e4596880e
Devesh Kumar Srivastava <devesh.kr.srivastava@gmail.com>
2020-06-06 23:24:21 +0530

    adding gitignore file

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 
  const app = express();
-const port = 8000;
+const port = 3003;
 
 app.get("/",(req,res)=>{
+    res.send("Hello world")
     console.log("Connection successfull")
 })
 console.log("this is my page");
 app.listen(port,()=> console.log(`The running port is ${port}`) )
+
+
